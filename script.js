5188342e56a6f3d8db52a2697c4c270992083168
MarceloA <marcelo.a.altamura@gmail.com>
2021-04-15 15:03:02 -0300

    tp terminado

--- script.js
+++ script.js
@@ -9,7 +9,46 @@ const classNames = {
   const list = document.getElementById('todo-list')
   const itemCountSpan = document.getElementById('item-count')
   const uncheckedCountSpan = document.getElementById('unchecked-count')
+  const tabla = document.getElementById('cuerpoToDo')
   
   function addTodo() {
-    alert('Boton Add TODO clickeado!')
+    let tarea = window.prompt("Ingrese la tarea", "");
+    if (tarea != null){
+      if(tarea != ""){
+        addCells(tarea);
+        counter();
+        countChecked();
+      }else{
+        alert("Error!");
+      }
+    }
+  }
+  function addCells(tarea){
+    let fila = tabla.insertRow(0);
+    let celdaTarea = fila.insertCell(0);
+    let celdaCheck = fila.insertCell(1);
+    celdaTarea.append(tarea);
+    celdaCheck.append(createCheckbox());
+
+  }
+  function countChecked(){
+    chkbox = document.getElementsByClassName(classNames.TODO_CHECKBOX)
+    uncheckedCountSpan.innerHTML = 0;
+    for (let index = 0; index < chkbox.length; index++) {
+      if (chkbox[index].checked == false) {
+        uncheckedCountSpan.innerHTML ++
+      }
+      
+    }
+  }
+  function createCheckbox(){
+    checkbox = document.createElement("input");
+    checkbox.type = "checkbox"
+    checkbox.className = classNames.TODO_CHECKBOX
+    checkbox.checked = false;
+    return checkbox
+  }
+
+  function counter(){
+    itemCountSpan.innerHTML++
   }
